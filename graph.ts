85cd4a5ef32664c71f8ec58312e337b5e473ce81
Max Schmidt <max.schmidt@outlook.de>
2023-04-06 12:28:25 +0200

    Refactor graph initialization to use array instead of map in Graph constructor

--- graph.ts
+++ graph.ts
@@ -1,27 +1,35 @@
-import { verbose } from "./main.ts";
 import { format } from "duration";
 import { colors } from "cliffy";
 export class Graph {
   size: number;
-  nodes: Map<number, Set<number>> = new Map();
+  nodes: number[][];
+  verbose: boolean;
 
-  constructor(graph: string) {
-    const start = performance.now();
+  constructor(graph: string, verbose: boolean) {
+    this.verbose = verbose;
+    let start = performance.now();
     const data = Deno.readTextFileSync(`./graphs/${graph}.txt`);
     let end = performance.now();
-    verbose && this.logTime("File read in", start, end);
+    this.verbose && this.logTime("File read in", start, end);
+    start = performance.now();
     const lines = data.split("\r\n");
     this.size = +lines.shift()!;
-    for (let i = 0; i < this.size; i++) {
-      this.nodes.set(i, new Set());
-    }
+    end = performance.now();
+    this.verbose &&
+      this.logTime("Lines split and size initialized in", start, end);
+    start = performance.now();
+    this.nodes = Array.from({ length: this.size }, () => []);
+    end = performance.now();
+    this.verbose && this.logTime("Graph filled in", start, end);
+    start = performance.now();
     for (const line of lines) {
       const [from, to] = line.split("\t").map((node) => +node);
-      this.nodes.get(from)!.add(to);
-      this.nodes.get(to)!.add(from);
+      if (!this.nodes[from].includes(to)) this.nodes[from].push(to);
+      if (!this.nodes[to].includes(from)) this.nodes[to]!.push(from);
     }
+
     end = performance.now();
-    verbose && this.logTime("Graph built in", start, end);
+    this.verbose && this.logTime("Graph built in", start, end);
   }
 
   get subGraphs() {
@@ -36,7 +44,7 @@ export class Graph {
       }
     }
     const end = performance.now();
-    verbose && this.logTime("Subgraph created in", start, end);
+    this.verbose && this.logTime("Subgraphs created in", start, end);
     return subgraphs;
   }
 
@@ -44,7 +52,7 @@ export class Graph {
     const stack = [start];
     while (stack.length) {
       const node = stack.pop()!;
-      for (const neighbor of this.nodes.get(node!)!) {
+      for (const neighbor of this.nodes[node]!) {
         if (!visited[neighbor]) {
           visited[neighbor] = 1;
           stack.push(neighbor);
