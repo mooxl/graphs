ffa42c1182bbdd9824e829f74cb3f9774a32e3d8
Max Schmidt <max.schmidt@outlook.de>
2023-05-11 22:23:49 +0200

    Add nearest neighbor and TSP algorithms to Graph class

--- graph.ts
+++ graph.ts
@@ -3,6 +3,11 @@ import { colors } from "cliffy";
 import { BinaryHeap } from "collections/binary_heap.ts";
 
 type Edge = { from: number; to: number; weight: number };
+
+type PartialTour = {
+  path: number[];
+  lowerBound: number;
+};
 export class Graph {
   size: number;
   nodes: Edge[][];
@@ -84,11 +89,90 @@ export class Graph {
     return setId[node];
   }
 
+  get nearestNeighbor() {
+    const start = performance.now();
+    const visited = new Uint8Array(this.size);
+    const path: Edge[] = [];
+    visited[0] = 1;
+    while (visited.includes(0)) {
+      let nearestNeighbor: Edge | undefined;
+      for (const edge of this.nodes[path[path.length - 1]?.to || 0]) {
+        if (!visited[edge.to]) {
+          if (!nearestNeighbor || edge.weight < nearestNeighbor.weight) {
+            nearestNeighbor = edge;
+          }
+        }
+      }
+      path.push(nearestNeighbor!);
+      visited[nearestNeighbor!.to] = 1;
+    }
+    path.push(
+      this.nodes[path[path.length - 1].to].find(
+        (edge) => edge.to === path[0].from
+      )!
+    );
+    this.logTime("Path created in", start, performance.now());
+    return path;
+  }
+
+  get tsp() {
+    const start = performance.now();
+    const nodes = Array.from({ length: this.size }, (_, i) => i);
+    let minCost = Infinity;
+    let minPath: Edge[] = [];
+    const permutations = this.permute(nodes.slice(1));
+    for (const perm of permutations) {
+      const path = [nodes[0], ...perm, nodes[0]];
+      let cost = 0;
+      for (let i = 0; i < path.length - 1; i++) {
+        const edge = this.nodes[path[i]].find((e) => e.to === path[i + 1])!;
+        cost += edge.weight;
+      }
+      if (cost < minCost) {
+        minCost = cost;
+        minPath = path
+          .map((n, i) => ({
+            from: n,
+            to: path[i + 1],
+            weight:
+              this.nodes[n].find((e) => e.to === path[i + 1])?.weight || 0,
+          }))
+          .slice(0, -1);
+      }
+    }
+    this.logTime("Optimal path found in", start, performance.now());
+    return minPath;
+  }
+
+  private permute(nodes: number[]): number[][] {
+    const result: number[][] = [];
+    const counters = new Array(nodes.length).fill(0);
+    result.push([...nodes]);
+    let i = 0;
+    while (i < nodes.length) {
+      if (counters[i] < i) {
+        if (i % 2 === 0) {
+          [nodes[0], nodes[i]] = [nodes[i], nodes[0]];
+        } else {
+          [nodes[counters[i]], nodes[i]] = [nodes[i], nodes[counters[i]]];
+        }
+        result.push([...nodes]);
+        counters[i]++;
+        i = 0;
+      } else {
+        counters[i] = 0;
+        i++;
+      }
+    }
+    return result;
+  }
+
   private dfs(start: number, visited: Uint8Array) {
     const stack = [start];
     const subgraph = [];
     while (stack.length) {
       const node = stack.pop()!;
+      visited[node] = 1;
       subgraph.push(node);
       for (const edge of this.nodes[node]!) {
         if (!visited[edge.to]) {
